79c546e0daa57a9950822d8879b635f904981955
VonC <vonc@laposte.net>
2020-06-21 12:16:47 +0200

    version structure

---
+++ version/version.go
@@ -0,0 +1,31 @@
+package version
+
+import (
+	"fmt"
+)
+
+var (
+	// Version semver for syncrepos (set through go build -ldflags)
+	Version string
+	// BuildUser is the user login who initiated the build (set through go build -ldflags)
+	BuildUser string
+	// GitTag is the result of git describe (set through go build -ldflags)
+	GitTag string
+	// ExeDir is the folder where the executable has been launched
+	ExeDir string
+	// BuildDate is the date of build (set through go build -ldflags)
+	BuildDate string
+	// StartDate is the date when the executable has been launched
+	StartDate string
+)
+
+// String displays all the version values
+func String() string {
+	res := ""
+	res = res + fmt.Sprintf("Git Tag   : %s\n", GitTag)
+	res = res + fmt.Sprintf("Build User: %s\n", BuildUser)
+	res = res + fmt.Sprintf("Version   : %s\n", Version)
+	res = res + fmt.Sprintf("BuildDate : %s\n", BuildDate)
+	res = res + fmt.Sprintf("ExeDir    : %s\n", ExeDir)
+	return res
+}
