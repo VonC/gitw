10cca8589ee180ae948feea26675f58c4292f3d1
VonC <vonc@laposte.net>
2021-05-30 01:12:02 +0200

    internal/shell/shell_windows.go: GetParentPS for Windows

--- internal/shell/shell_windows.go
+++ internal/shell/shell_windows.go
@@ -5,6 +5,9 @@ package shell
 import (
 	"fmt"
 	"gitw/internal/syscall"
+	"gitw/internal/xregexp"
+	"os"
+	"regexp"
 )
 
 func CleanOldBashFiles(verbose bool) error {
@@ -17,4 +20,27 @@ func CleanOldBashFiles(verbose bool) error {
 		return fmt.Errorf("error: unable to clean old /tmp/bash files: '%+v', serr '%s'", err, serr.String())
 	}
 	return nil
+}
+
+// https://regex101.com/r/67F20E/1
+var repid = regexp.MustCompile(`(?m)^(?P<cmd>.*?)\s+\d+\s+(?P<pid>\d+)$`)
+
+func GetParentPS(apid Pid) (*Ps, error) {
+	spid := string(apid)
+	if spid == "" {
+		spid = fmt.Sprintf("%d", os.Getpid())
+	}
+	serr, sout, err := syscall.ExecCmd(fmt.Sprintf(`wmic process get processid,parentprocessid,executablepath|C:\Windows\System32\find.exe "%s"`, spid))
+	if err != nil || serr.String() != "" {
+		err := fmt.Errorf("error: unable to get PID of current bash session: '%+v', serr '%s'", err, serr.String())
+		return nil, err
+	}
+	var p *Ps
+	matches := xregexp.FindNamedMatches(repid, sout.String())
+	if len(matches) > 0 {
+		p = &Ps{pid: Pid(matches["pid"]), cmd: matches["cmd"]}
+	} else {
+		return nil, fmt.Errorf("error: unable to get ps from: '%s'", sout.String())
+	}
+	return p, err
 }
