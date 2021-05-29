5902ac6a3ab1634a1b2933ec3256132b4450d4a2
VonC <vonc@laposte.net>
2021-05-29 23:30:02 +0200

    internal/shell/shell_windows.go: fix error message

--- internal/shell/shell_windows.go
+++ internal/shell/shell_windows.go
@@ -14,7 +14,7 @@ func CleanOldBashFiles(verbose bool) error {
 		fmt.Printf("sout(%s)='%s'\n", cmd, sout.String())
 	}
 	if err != nil || sout.String() != "" || serr.String() != "" {
-		return fmt.Errorf("Error: unable to clean old /tmp/bash files: '%+v', serr '%s'", err, serr.String())
+		return fmt.Errorf("error: unable to clean old /tmp/bash files: '%+v', serr '%s'", err, serr.String())
 	}
 	return nil
 }
