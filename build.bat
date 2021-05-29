18a7314ee064ce2b3ffb50e044e7fb97ed5e71e1
VonC <vonc@laposte.net>
2021-05-29 22:12:51 +0200

    build.bat: use buildversion to generate version.txt

--- build.bat
+++ build.bat
@@ -5,26 +5,23 @@ for %%i in ("%~dp0.") do SET "script_dir=%%~fi"
 cd "%script_dir%"
 for %%i in ("%~dp0.") do SET "dirname=%%~ni"
 
-rem https://medium.com/@joshroppo/setting-go-1-5-variables-at-compile-time-for-versioning-5b30a965d33e
-for /f %%i in ('git describe --long --tags --dirty --always') do set gitver=%%i
-set VERSION=v0.0.1
-
-rem https://stackoverflow.com/a/1445724/6309
-: Sets the proper date and time stamp with 24Hr Time for log file naming
-: convention
-
-SET HOUR=%time:~0,2%
-if "%HOUR:~0,1%" == " " (SET HOUR=0%HOUR:~1,1%) 
-rem SET dtStamp9=%date:~-4%%date:~4,2%%date:~7,2%_0%time:~1,1%%time:~3,2%%time:~6,2% 
-SET dtStamp24=%date:~-4%%date:~3,2%%date:~0,2%-%HOUR%%time:~3,2%%time:~6,2%
-
-rem if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
-SET dtStamp=%dtStamp24%
+if not exist buildversion.exe (
+    go build version/cmd/buildversion.go
+    if errorlevel 1 (
+        echo ERROR BUILD BUILD VERSION 1>&2
+        exit /b 1
+    )
+)
+buildversion.exe
+if errorlevel 1 (
+    echo ERROR RUN BUILD VERSION 1>&2
+    exit /b 1
+)
 
 if "%1" == "amd" (
     set GOARCH=amd64
     set GOOS=linux
-    go build -ldflags "-X %dirname%/version.GitTag=%gitver% -X %dirname%/version.BuildUser=%USERNAME% -X %dirname%/version.Version=%VERSION% -X %dirname%/version.BuildDate=%dtStamp%"
+    go build
     if errorlevel 1 (
         echo ERROR BUILD 1>&2
         exit /b 1
@@ -32,5 +29,9 @@ if "%1" == "amd" (
     goto:eof
 )
 
-go build -ldflags "-X %dirname%/version.GitTag=%gitver% -X %dirname%/version.BuildUser=%USERNAME% -X %dirname%/version.Version=%VERSION% -X %dirname%/version.BuildDate=%dtStamp%"
+go build
+if errorlevel 1 (
+    echo ERROR BUILD 1>&2
+    exit /b 1
+)
 if "%1" neq "" ( %dirname% %* )
