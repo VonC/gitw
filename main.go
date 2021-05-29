57a38d69895401f480c53a8f22b63b342a191e24
VonC <vonc@laposte.net>
2021-05-30 01:15:41 +0200

    main.go: getBashSession uses shell.GetBashPID() and shell.GetPSStartDate(pid)

--- main.go
+++ main.go
@@ -410,8 +410,6 @@ func (b *bash) getUser() *user {
 	return u
 }
 
-type pid string
-
 type bash struct {
 	file    string
 	user    *user
@@ -420,105 +418,22 @@ type bash struct {
 
 type session struct {
 	date time.Time
-	pid  pid
-}
-
-type ps struct {
-	pid pid
-	cmd string
-}
-
-func getBashPID() (pid, error) {
-	p, err := getPS(pid(""))
-	// fmt.Printf("ps ='%+v'\nerr='%+v'\n", p, err)
-	if err != nil {
-		return pid(""), err
-	}
-	for !p.isGitW() {
-		p, err = getPS(p.pid)
-		// fmt.Printf("psng ='%+v'\nerr='%+v'\n", p, err)
-		if err != nil {
-			return pid(""), err
-		}
-	}
-	var bpid pid
-	for p.isGitW() {
-		bpid = p.pid
-		p, err = getPS(p.pid)
-		// fmt.Printf("psig ='%+v'\nerr='%+v'\n", p, err)
-		if err != nil {
-			return pid(""), err
-		}
-	}
-	//fmt.Println(">>>" + string(bpid))
-	return bpid, nil
-}
-
-func (p *ps) isGitW() bool {
-	return strings.Contains(p.cmd, "gitw")
-}
-
-//   PPID
-// 64443 ps -p 64447 -o ppid,cmd=
-var repid = regexp.MustCompile(`(?m)^\s*?(?P<pid>\d+)\s+(?P<cmd>.*?)$`)
-
-func getPS(apid pid) (*ps, error) {
-	spid := string(apid)
-	if spid == "" {
-		spid = "$(echo $$)"
-	}
-	serr, sout, err := syscall.ExecCmd(fmt.Sprintf("ps -p %s -o ppid,cmd=", spid))
-	if err != nil || serr.String() != "" {
-		err := fmt.Errorf("Error: unable to get PID of current bash session: '%+v', serr '%s'", err, serr.String())
-		return nil, err
-	}
-	// fmt.Println("===" + sout.String())
-	var p *ps
-	matches := findNamedMatches(repid, sout.String())
-	if len(matches) > 0 {
-		p = &ps{pid: pid(matches["pid"]), cmd: matches["cmd"]}
-	} else {
-		return nil, fmt.Errorf("Error: unable to get ps from: '%s'", sout.String())
-	}
-	return p, err
+	pid  shell.Pid
 }
 
 func getBashSession() (*session, error) {
-	pid, err := getBashPID()
+	pid, err := shell.GetBashPID()
 	if err != nil {
 		return nil, err
 	}
-	cmd := `ps -ewo pid,lstart|grep -E "^\s+?%s "`
-	cmd = fmt.Sprintf(cmd, pid)
-	serr, sout, err := syscall.ExecCmd(cmd)
-	if verbose {
-		fmt.Printf("sout(%s)='%s'\n", cmd, sout.String())
-	}
+	date, err := shell.GetPSStartDate(pid, verbose)
 	if err != nil {
-		return nil, fmt.Errorf("Error: unable to get bash time: '%+v', serr '%s'", err, serr.String())
-	}
-	//  25301 Wed Aug 28 10:16:24 2019 -bash
-	resession := regexp.MustCompile(`(?m)^\s*?\d+\s+?(?P<date>.*?)$`)
-	matches := findNamedMatches(resession, sout.String())
-	if len(matches) > 0 {
-		if verbose {
-			fmt.Printf("Session date '%s'\n", matches["date"])
-		}
-	} else {
-		return nil, fmt.Errorf("Error: unable to get bash time from: '%s'", sout.String())
-	}
-	if verbose {
-		fmt.Printf("stime='%s'\n", matches["date"])
-	}
-	// Wed Aug 28 10:16:24 2019
-	date, err := time.Parse("Mon Jan 2 15:04:05 2006", matches["date"])
-	if err != nil {
-		return nil, fmt.Errorf("Error: unable to parse time: '%+v', serr '%s'", matches["date"], err)
+		return nil, err
 	}
 	if verbose {
 		fmt.Printf("date='%+v'\n", date)
 	}
-	s := &session{pid: pid, date: date}
+	s := &session{pid: pid, date: *date}
 	return s, nil
 }
 
